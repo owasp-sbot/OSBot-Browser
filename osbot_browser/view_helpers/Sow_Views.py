5ac8a43f3acb0bca7fa9260794f74f75394d2af6
Dinis Cruz <dinis.cruz@owasp.org>
2020-01-10 18:44:44 +0000

    added Json python data transfer

--- osbot_browser/view_helpers/Sow_Views.py
+++ osbot_browser/view_helpers/Sow_Views.py
@@ -16,6 +16,19 @@ class Sow_Views:
 
     @staticmethod
     def default(team_id=None, channel=None, params=None, no_render=False, headless=True):
+
         maps = Sow(headless)
         maps.load_page(True)
+
+        issue_data = {
+            "requirement_number"   : "1A - Determine File Type"                                 ,
+            "requirement_language" : "N/A"                                                      ,
+            "verification_method"  : "Test"                                                     ,
+            "setup"                : ""                                                         ,
+            "execution_steps"      : "<ol><li></li><li></li><li></li><li></li><li></li></ol>"   ,
+            "expected_result"      : ''                                                         ,
+            "compliance"           : '&lt;Compliant | Partially Compliant | Non-Compliant &gt;' ,
+            "results"              : "{0}".format(params)
+        }
+        maps.invoke_js('set_data', issue_data)
         return maps.send_screenshot_to_slack('not-used', channel)
--- test/view_helpers/test_Sow_Views.py
+++ test/view_helpers/test_Sow_Views.py
@@ -4,7 +4,6 @@ from osbot_aws.apis.Lambda import Lambda
 from pbx_gs_python_utils.utils.Dev import Dev
 
 from osbot_browser.browser.Browser_Lamdba_Helper import Browser_Lamdba_Helper
-from osbot_browser.view_helpers.Maps_Views import Maps_Views
 from gw_bot.Deploy import Deploy
 from osbot_browser.view_helpers.Sow_Views import Sow_Views
 
@@ -25,10 +24,15 @@ class Test_Sow_Views(TestCase):
             Browser_Lamdba_Helper().save_png_data(self.png_data)
 
     def test_default(self):
-        self.png_data = self.sow_views.default(headless=False)#,channel='DJ8UA0RFT')
+        self.png_data = self.sow_views.default(headless=False)
+
+
+    def test_update_lambda(self):
+        Deploy().setup().deploy_lambda__browser()
 
     def test_using_lambda(self):
-        payload = {"params": ['render','/sow/simple.html',0,0,600,50]}
+        payload = {"params": ['sow', 'default']}
         self.lambda_name = 'osbot_browser.lambdas.lambda_browser'
         self.lambda_browser = Lambda(self.lambda_name)
-        self.png_data  = self.lambda_browser.invoke(payload)
+        self.result = self.lambda_browser.invoke(payload)
+        #self.png_data  = self.lambda_browser.invoke(payload)
