06cbb643b18b10328402c5408c5ec12e183dd385
Dinis Cruz <dinis.cruz@owasp.org>
2020-02-16 21:26:50 +0000

    minor fix

--- test_QA/lambdas/test_google_chart.py
+++ test_QA/lambdas/test_google_chart.py
@@ -10,16 +10,17 @@ class test_google_charts(Test_Helper):
 
     def setUp(self):
         super().setUp()
-        self.headless    = False
+        self.headless    = True
         self.png_data    = None
         self.lambda_name = 'osbot_browser.lambdas.google_chart'
         self._lambda     = Lambda(self.lambda_name)
         self.chart_type  = 'LineChart'
         self.options     = {'title'    : 'An Chart (in Lambda)',
                             'curveType': 'function',
-                            'legend'   : {'position': 'bottom'}}
+                            'legend'   : {'position': 'bottom'},
+                            }
         self.data        = [['Year', 'Line 1', 'Line 2','Line 3'],
-                            ['2004', 500, 400,30],
+                            ['2004', 100, 400,30],
                             ['2005', 1170, 460,400],
                             ['2006', 660, 1120,100],
                             ['2007', 1030, 540,200],
@@ -27,10 +28,10 @@ class test_google_charts(Test_Helper):
                             ['2009', 1030, 540,100]]
         self.params       = { 'chart_type':self.chart_type, 'options': self.options , 'data': self.data }
 
-    def tearDown(self):
-        super().tearDown()
-        if self.png_data:
-            Browser_Lamdba_Helper().save_png_data(self.png_data)
+    # def tearDown(self):
+    #     super().tearDown()
+    #     if self.png_data:
+    #         Browser_Lamdba_Helper().save_png_data(self.png_data)
 
     def test_invoke_directly(self):
         #params = { 'chart_type':self.chart_type, 'options': self.options , 'data': self.data}
